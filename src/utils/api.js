f66ef5cad9871dd59455c671da13b3cea86c7162
HaroldF415 <harold.f415@gmail.com>
2023-04-22 17:19:42 -0400

    removed console.log

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = process.env.REACT_APP_API_URL;
-console.log(API);
+
 export const fetchTransactions = async () => {
   try {
     const response = await axios.get(`${API}/transactions`);
