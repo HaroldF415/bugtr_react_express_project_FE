6690e7902de7cb661a35578abbea2520564ec42f
HaroldF415 <harold.f415@gmail.com>
2023-04-22 17:20:02 -0400

    added some styling and mild responsiveness

--- src/components/Transactions/TransactionsIndex/Transactions.jsx
+++ src/components/Transactions/TransactionsIndex/Transactions.jsx
@@ -14,34 +14,38 @@ const Transactions = () => {
   }, []);
 
   return (
-    <div className="container">
+    <div className="container mx-auto px-4">
       <div className="py-4">
-        <h1>Transactions</h1>
-        <table className="table border shadow">
-          <thead className="thead-dark">
+        <h1 className="text-2xl font-bold mb-4">Transactions</h1>
+        <table className="table-auto w-full">
+          <thead className="bg-gray-700 text-white">
             <tr>
-              <th scope="col">#</th>
-              <th scope="col">Date</th>
-              <th scope="col">Description</th>
-              <th scope="col">Amount</th>
-              <th>Action</th>
+              <th className="px-4 py-2">#</th>
+              <th className="px-4 py-2">Date</th>
+              <th className="px-4 py-2">From</th>
+              <th className="px-4 py-2">Amount</th>
+              <th className="px-4 py-2">Category</th>
+              <th className="px-4 py-2">Action</th>
             </tr>
           </thead>
           <tbody>
             {transactions.map((transaction, index) => (
-              <tr>
-                <th scope="row">{index + 1}</th>
-                <td>{transaction.date}</td>
-                <td>{transaction.description}</td>
-                <td>{transaction.amount}</td>
-                <td>
-                  <Link className="btn btn-primary mr-2" to={`/transactions/${transaction.id}`}>
+              <tr key={transaction.id} className={index % 2 === 0 ? "bg-gray-100" : "bg-white"}>
+                <th className="px-4 py-2" scope="row">
+                  {index + 1}
+                </th>
+                <td className="px-4 py-2">{transaction.date}</td>
+                <td className="px-4 py-2">{transaction.from}</td>
+                <td className="px-4 py-2">{transaction.amount}</td>
+                <td className="px-4 py-2">{transaction.category}</td>
+                <td className="px-4 py-2">
+                  <Link className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded mr-1" to={`/transactions/${transaction.id}`}>
                     View
                   </Link>
-                  <Link className="btn btn-outline-primary mr-2" to={`/transactions/edit/${transaction.id}`}>
+                  <Link className="bg-green-500 hover:bg-green-700 text-white font-bold py-1 px-2 rounded mr-1" to={`/transactions/edit/${transaction.id}`}>
                     Edit
                   </Link>
-                  <Link className="btn btn-danger" to={`/transactions/delete/${transaction.id}`}>
+                  <Link className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded" to={`/transactions/delete/${transaction.id}`}>
                     Delete
                   </Link>
                 </td>
