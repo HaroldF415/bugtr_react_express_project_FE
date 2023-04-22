f1c71f57f8a8b5962ed1b2d1ff3bb142ff9afded
HaroldF415 <harold.f415@gmail.com>
2023-04-22 17:31:41 -0400

    human readable date plus uppercase on some properties. formating continues

--- src/components/Transactions/TransactionsIndex/Transactions.jsx
+++ src/components/Transactions/TransactionsIndex/Transactions.jsx
@@ -2,6 +2,45 @@ import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { fetchTransactions } from "../../../utils/api";
 
+const firstLetterUppercase = (string) => {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+};
+
+const humanReadableDate = (date) => {
+  const dateObject = new Date(date);
+  return dateObject.toLocaleDateString("en-US", {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+};
+
+const Transaction = ({ transaction, index }) => {
+  return (
+    <tr className={index % 2 === 0 ? "bg-gray-100" : "bg-white"}>
+      <th className="px-4 py-2" scope="row">
+        {index + 1}
+      </th>
+      <td className="px-4 py-2">{humanReadableDate(transaction.date)}</td>
+      <td className="px-4 py-2">{firstLetterUppercase(transaction.from)}</td>
+      <td className="px-4 py-2">$ {transaction.amount}.00</td>
+      <td className="px-4 py-2">{firstLetterUppercase(transaction.category)}</td>
+      <td className="px-4 py-2">
+        <Link className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded mr-1" to={`/transactions/${transaction.id}`}>
+          View
+        </Link>
+        <Link className="bg-green-500 hover:bg-green-700 text-white font-bold py-1 px-2 rounded mr-1" to={`/transactions/edit/${transaction.id}`}>
+          Edit
+        </Link>
+        <Link className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded" to={`/transactions/delete/${transaction.id}`}>
+          Delete
+        </Link>
+      </td>
+    </tr>
+  );
+};
+
 const Transactions = () => {
   const [transactions, setTransactions] = useState([]);
 
@@ -30,26 +69,7 @@ const Transactions = () => {
           </thead>
           <tbody>
             {transactions.map((transaction, index) => (
-              <tr key={transaction.id} className={index % 2 === 0 ? "bg-gray-100" : "bg-white"}>
-                <th className="px-4 py-2" scope="row">
-                  {index + 1}
-                </th>
-                <td className="px-4 py-2">{transaction.date}</td>
-                <td className="px-4 py-2">{transaction.from}</td>
-                <td className="px-4 py-2">{transaction.amount}</td>
-                <td className="px-4 py-2">{transaction.category}</td>
-                <td className="px-4 py-2">
-                  <Link className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded mr-1" to={`/transactions/${transaction.id}`}>
-                    View
-                  </Link>
-                  <Link className="bg-green-500 hover:bg-green-700 text-white font-bold py-1 px-2 rounded mr-1" to={`/transactions/edit/${transaction.id}`}>
-                    Edit
-                  </Link>
-                  <Link className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded" to={`/transactions/delete/${transaction.id}`}>
-                    Delete
-                  </Link>
-                </td>
-              </tr>
+              <Transaction key={transaction.id} transaction={transaction} index={index} />
             ))}
           </tbody>
         </table>
