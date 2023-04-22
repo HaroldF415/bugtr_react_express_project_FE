9b65f84f0e56759f2a8a26d6f5c0438b48ad6233
HaroldF415 <harold.f415@gmail.com>
2023-04-22 17:07:49 -0400

    updated the transactions route to display a default view

--- src/components/Transactions/TransactionsIndex/Transactions.jsx
+++ src/components/Transactions/TransactionsIndex/Transactions.jsx
@@ -1,5 +1,57 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { fetchTransactions } from "../../../utils/api";
+
 const Transactions = () => {
-  return null;
+  const [transactions, setTransactions] = useState([]);
+
+  useEffect(() => {
+    const fetchAPI = async () => {
+      setTransactions(await fetchTransactions());
+    };
+
+    fetchAPI();
+  }, []);
+
+  return (
+    <div className="container">
+      <div className="py-4">
+        <h1>Transactions</h1>
+        <table className="table border shadow">
+          <thead className="thead-dark">
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Date</th>
+              <th scope="col">Description</th>
+              <th scope="col">Amount</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {transactions.map((transaction, index) => (
+              <tr>
+                <th scope="row">{index + 1}</th>
+                <td>{transaction.date}</td>
+                <td>{transaction.description}</td>
+                <td>{transaction.amount}</td>
+                <td>
+                  <Link className="btn btn-primary mr-2" to={`/transactions/${transaction.id}`}>
+                    View
+                  </Link>
+                  <Link className="btn btn-outline-primary mr-2" to={`/transactions/edit/${transaction.id}`}>
+                    Edit
+                  </Link>
+                  <Link className="btn btn-danger" to={`/transactions/delete/${transaction.id}`}>
+                    Delete
+                  </Link>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default Transactions;
---
+++ src/utils/api.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const API = process.env.REACT_APP_API_URL;
+console.log(API);
+export const fetchTransactions = async () => {
+  try {
+    const response = await axios.get(`${API}/transactions`);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+};
