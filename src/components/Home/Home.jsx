c0de6578c0e6dfedf0212a3483c65b2e7f2e2620
HaroldF415 <harold.f415@gmail.com>
2023-04-22 18:12:17 -0400

    created a button to display the transactionsIndex page. sort of like a landing page. more to come

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,5 +1,13 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
-  return null;
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <Link to="/transactions">
+        <button className="bg-teal-100 hover:bg-white text-teal-800 text-7xl rounded-full py-24 px-48 shadow-md hover:shadow-2xl transition duration-500">View Transactions</button>
+      </Link>
+    </div>
+  );
 };
 
 export default Home;
