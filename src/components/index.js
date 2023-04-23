47b59f7e81c68b2dc2e374825c781bad9a99d653
HaroldF415 <harold.f415@gmail.com>
2023-04-22 20:03:17 -0400

    imported ShowTransaction compoenent for barrel exporting

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@
 import NavBar from "./common/NavBar/NavBar";
 import Home from "./Home/Home";
 import Transactions from "./Transactions/TransactionsIndex/Transactions";
+import ShowTransaction from "./Transactions/ShowTransaction/ShowTransaction";
 
-export { NavBar, Home, Transactions };
+export { NavBar, Home, Transactions, ShowTransaction };
