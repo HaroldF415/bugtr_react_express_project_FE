4cc0c08910e629db5df15c05420e02fb0d40a642
HaroldF415 <harold.f415@gmail.com>
2023-04-22 21:04:37 -0400

    updated my barrel export file to compensate for NewTransaction

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@ import NavBar from "./common/NavBar/NavBar";
 import Home from "./Home/Home";
 import Transactions from "./Transactions/TransactionsIndex/Transactions";
 import ShowTransaction from "./Transactions/ShowTransaction/ShowTransaction";
+import NewTransaction from "./Transactions/NewTransaction/NewTransaction";
 
-export { NavBar, Home, Transactions, ShowTransaction };
+export { NavBar, Home, Transactions, ShowTransaction, NewTransaction };
