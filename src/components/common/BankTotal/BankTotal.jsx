3350f9e3c7f74fe10bc51185fa9b751355399968
HaroldF415 <harold.f415@gmail.com>
2023-04-24 05:29:17 -0400

    updated logic for bankTotal component

--- src/components/common/BankTotal/BankTotal.jsx
+++ src/components/common/BankTotal/BankTotal.jsx
@@ -1,25 +1,12 @@
-import { useEffect } from "react";
-import { useTransactions } from "../../../hooks/useTransactions";
-import { humanReadableAmount } from "../../../utils/formattingHelpers";
+import useBankTotal from "../../../hooks/useBankTotal";
+import { humanReadableAmount, setBankTotalColor } from "../../../utils/formattingHelpers";
 
 const BankTotal = () => {
-  const { transactions } = useTransactions();
-  let total = 0;
-  transactions.forEach((transaction) => {
-    if (transaction.transaction_type === "deposit") {
-      total += transaction.amount;
-    } else if (transaction.transaction_type === "withdrawal") {
-      total -= transaction.amount;
-    }
-  });
-
-  useEffect(() => {
-    document.title = `Bank Total: ${humanReadableAmount(total)}`;
-  }, [transactions, total]);
+  const total = useBankTotal();
 
   return (
     <div className="container mx-auto px-4 flex justify-center">
-      <h1 className="text-4xl">Bank Total {humanReadableAmount(total)}</h1>
+      <h1 className={`text-4xl ${setBankTotalColor(total)}`}>Bank Total {humanReadableAmount(total)}</h1>
     </div>
   );
 };
